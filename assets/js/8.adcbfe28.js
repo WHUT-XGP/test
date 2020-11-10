1b743c3a8e6f1dbdf5b10ab92fe3b7763dcf143a
whutfaker <1312976124@qq.com>
2020-11-10 11:45:10 +0800

    deploy

---
+++ assets/js/8.adcbfe28.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{358:function(t,e,n){"use strict";n.r(e);var s=n(42),l=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("p",[this._v("马上开始和AX一起学习Vue3")])])}),[],!1,null,null,null);e.default=l.exports}}]);
